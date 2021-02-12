ebd57889e6f2b4bfa854840ad260bc3e705e8436
unlomtrois <vadim.kuz02@gmail.com>
2021-02-12 13:54:18 +0300

    uncomment path for frontend part

--- app/__init__.py
+++ app/__init__.py
@@ -10,7 +10,7 @@ app.register_blueprint(api, url_prefix="/api")
 
 # Serve React App
 @app.route("/", defaults={"path": ""})
-# @app.route("/<path:path>")
+@app.route("/<path:path>")
 def serve(path):
     if path != "" and os.path.exists(app.static_folder + "/" + path):
         return send_from_directory(app.static_folder, path)
